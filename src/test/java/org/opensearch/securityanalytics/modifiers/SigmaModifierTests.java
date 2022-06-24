76afb6ad3efdd4e28d3fb681260c4a759fe2e616
Subhobrata Dey <sbcd90@gmail.com>
2022-06-24 01:18:17 +0000

    add unit tests part-4
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- src/test/java/org/opensearch/securityanalytics/modifiers/SigmaModifierTests.java
+++ src/test/java/org/opensearch/securityanalytics/modifiers/SigmaModifierTests.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright OpenSearch Contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package org.opensearch.securityanalytics.modifiers;
 
 import org.apache.commons.lang3.tuple.Pair;
