a6efc9629e424616ba90c4ac9237f399943bc1f9
Subhobrata Dey <sbcd90@gmail.com>
2022-07-06 02:09:42 +0000

    integrate Sigma rules parser-part2
    Signed-off-by: Subhobrata Dey <sbcd90@gmail.com>

--- src/main/generated/org/opensearch/securityanalytics/rules/condition/ConditionBaseListener.java
+++ src/main/generated/org/opensearch/securityanalytics/rules/condition/ConditionBaseListener.java
@@ -15,101 +15,101 @@ import org.antlr.v4.runtime.tree.TerminalNode;
  * of the available methods.
  */
 public class ConditionBaseListener implements ConditionListener {
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterStart(ConditionParser.StartContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitStart(ConditionParser.StartContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterOrExpression(ConditionParser.OrExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitOrExpression(ConditionParser.OrExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterAndExpression(ConditionParser.AndExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitAndExpression(ConditionParser.AndExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterNotExpression(ConditionParser.NotExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitNotExpression(ConditionParser.NotExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterParenExpression(ConditionParser.ParenExpressionContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitParenExpression(ConditionParser.ParenExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterStart(ConditionParser.StartContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitStart(ConditionParser.StartContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterOrExpression(ConditionParser.OrExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitOrExpression(ConditionParser.OrExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterAndExpression(ConditionParser.AndExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitAndExpression(ConditionParser.AndExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterNotExpression(ConditionParser.NotExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitNotExpression(ConditionParser.NotExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterParenExpression(ConditionParser.ParenExpressionContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitParenExpression(ConditionParser.ParenExpressionContext ctx) { }
 
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void enterEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void exitEveryRule(ParserRuleContext ctx) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitTerminal(TerminalNode node) { }
-	/**
-	 * {@inheritDoc}
-	 *
-	 * <p>The default implementation does nothing.</p>
-	 */
-	@Override public void visitErrorNode(ErrorNode node) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void enterEveryRule(ParserRuleContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void exitEveryRule(ParserRuleContext ctx) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitTerminal(TerminalNode node) { }
+    /**
+     * {@inheritDoc}
+     *
+     * <p>The default implementation does nothing.</p>
+     */
+    @Override public void visitErrorNode(ErrorNode node) { }
 }
--- src/main/generated/org/opensearch/securityanalytics/rules/condition/ConditionListener.java
+++ src/main/generated/org/opensearch/securityanalytics/rules/condition/ConditionListener.java
@@ -11,74 +11,74 @@ import org.antlr.v4.runtime.tree.ParseTreeListener;
  * {@link ConditionParser}.
  */
 public interface ConditionListener extends ParseTreeListener {
-	/**
-	 * Enter a parse tree produced by {@link ConditionParser#start}.
-	 * @param ctx the parse tree
-	 */
-	void enterStart(ConditionParser.StartContext ctx);
-	/**
-	 * Exit a parse tree produced by {@link ConditionParser#start}.
-	 * @param ctx the parse tree
-	 */
-	void exitStart(ConditionParser.StartContext ctx);
-	/**
-	 * Enter a parse tree produced by the {@code orExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void enterOrExpression(ConditionParser.OrExpressionContext ctx);
-	/**
-	 * Exit a parse tree produced by the {@code orExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void exitOrExpression(ConditionParser.OrExpressionContext ctx);
-	/**
-	 * Enter a parse tree produced by the {@code identOrSelectExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void enterIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx);
-	/**
-	 * Exit a parse tree produced by the {@code identOrSelectExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void exitIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx);
-	/**
-	 * Enter a parse tree produced by the {@code andExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void enterAndExpression(ConditionParser.AndExpressionContext ctx);
-	/**
-	 * Exit a parse tree produced by the {@code andExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void exitAndExpression(ConditionParser.AndExpressionContext ctx);
-	/**
-	 * Enter a parse tree produced by the {@code notExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void enterNotExpression(ConditionParser.NotExpressionContext ctx);
-	/**
-	 * Exit a parse tree produced by the {@code notExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void exitNotExpression(ConditionParser.NotExpressionContext ctx);
-	/**
-	 * Enter a parse tree produced by the {@code parenExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void enterParenExpression(ConditionParser.ParenExpressionContext ctx);
-	/**
-	 * Exit a parse tree produced by the {@code parenExpression}
-	 * labeled alternative in {@link ConditionParser#expression}.
-	 * @param ctx the parse tree
-	 */
-	void exitParenExpression(ConditionParser.ParenExpressionContext ctx);
+    /**
+     * Enter a parse tree produced by {@link ConditionParser#start}.
+     * @param ctx the parse tree
+     */
+    void enterStart(ConditionParser.StartContext ctx);
+    /**
+     * Exit a parse tree produced by {@link ConditionParser#start}.
+     * @param ctx the parse tree
+     */
+    void exitStart(ConditionParser.StartContext ctx);
+    /**
+     * Enter a parse tree produced by the {@code orExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void enterOrExpression(ConditionParser.OrExpressionContext ctx);
+    /**
+     * Exit a parse tree produced by the {@code orExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void exitOrExpression(ConditionParser.OrExpressionContext ctx);
+    /**
+     * Enter a parse tree produced by the {@code identOrSelectExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void enterIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx);
+    /**
+     * Exit a parse tree produced by the {@code identOrSelectExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void exitIdentOrSelectExpression(ConditionParser.IdentOrSelectExpressionContext ctx);
+    /**
+     * Enter a parse tree produced by the {@code andExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void enterAndExpression(ConditionParser.AndExpressionContext ctx);
+    /**
+     * Exit a parse tree produced by the {@code andExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void exitAndExpression(ConditionParser.AndExpressionContext ctx);
+    /**
+     * Enter a parse tree produced by the {@code notExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void enterNotExpression(ConditionParser.NotExpressionContext ctx);
+    /**
+     * Exit a parse tree produced by the {@code notExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void exitNotExpression(ConditionParser.NotExpressionContext ctx);
+    /**
+     * Enter a parse tree produced by the {@code parenExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void enterParenExpression(ConditionParser.ParenExpressionContext ctx);
+    /**
+     * Exit a parse tree produced by the {@code parenExpression}
+     * labeled alternative in {@link ConditionParser#expression}.
+     * @param ctx the parse tree
+     */
+    void exitParenExpression(ConditionParser.ParenExpressionContext ctx);
 }
